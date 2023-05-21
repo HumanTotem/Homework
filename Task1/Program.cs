4fbc119e76d66d06d47ce939abee877066668eff
Ivan Kulikov <human.totem1@gmail.com>
2023-05-21 11:04:42 +0300

    Решил задачу 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+
 Console.WriteLine("Введите чимсло 1: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите чимсло 2: ");
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите чимсло 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чимсло 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чимсло 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 && number2 == number3)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"Число {number1} самое большое");
+}
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine($"Число {number2} самое большое");
+}
+else if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine($"Число {number3} самое большое");
+}
+}
+
+
